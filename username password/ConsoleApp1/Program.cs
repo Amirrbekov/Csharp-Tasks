54c55eccc7bbc0974be82650d280fdee1b3dfec3
Amirrbekov <amirbekovaleh@gmail.com>
2023-04-18 21:54:57 +0400

    first commit

---
+++ username password/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿string username = "admin";
+string password = "123";
+bool k = false;
+
+do
+{
+    Console.Write("Please Enter Your Username: ");
+    string yusername = Console.ReadLine();
+    Console.Write("Please Enter Your Password: ");
+    string ypassword = Console.ReadLine();
+    if (yusername == username && ypassword == password) 
+    {
+        Console.WriteLine("Correct");
+        k = true; 
+    }
+    else Console.WriteLine("Username or password wrong");
+} while (!k);
