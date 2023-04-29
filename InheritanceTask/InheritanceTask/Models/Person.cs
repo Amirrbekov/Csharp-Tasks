4d9e77cc5f08a48aa740c3ea9446b92434bd8c8d
Amirrbekov <amirbekovaleh@gmail.com>
2023-04-29 18:38:23 +0400

    Inheritance Task

---
+++ InheritanceTask/InheritanceTask/Models/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace InheritanceTask.Models;
+
+internal class Person
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public int Age { get; set; }
+}
---
+++ InheritanceTask/InheritanceTask/Program.cs
@@ -0,0 +1,11 @@
+﻿using InheritanceTask.Models;
+
+namespace InheritanceTask;
+
+public static class Program
+{
+    public static void Main(String[] args)
+    {
+        Student student = new("Valeh", "Amirbekov", 18, 91, 58);
+    }
+}
