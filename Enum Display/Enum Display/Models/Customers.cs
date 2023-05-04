43de7c9c8a839b50d22997b1d14b18e870cd59f3
Amirrbekov <amirbekovaleh@gmail.com>
2023-05-04 23:20:09 +0400

    Enum Task

---
+++ Enum Display/Enum Display/Models/Customers.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace Enum_Display.Models;
+
+public enum Status : byte
+{
+    None = 0,
+
+    [Display(Name = "Aktif Kullanıcıları Listeler")]
+    Active = 1,
+
+    [Display(Name = "Pasif Kullanıcıları Listeler")]
+    Passive,
+
+    [Display(Name = "Silinmiş Kullanıcıları Listeler")]
+    Deleted,
+
+    [Display(Name = "Kaydını Dondurmuş Kullanıcıları Listeler")]
+    Holiday
+}
+
+public class Customers
+{
+    public Status Status { get; set; }
+    public void DisplayName()
+    {
+        var prop = typeof(Status).GetFields();
+        foreach (var field in prop)
+        {
+            var displayAtr = field.GetCustomAttribute(typeof(DisplayAttribute)) as DisplayAttribute;
+            if (displayAtr != null)
+            {
+                Console.WriteLine($"{field.Name} => {displayAtr.Name}");
+            }
+        }
+    }
+}
---
+++ Enum Display/Enum Display/Program.cs
@@ -0,0 +1,12 @@
+﻿using Enum_Display.Models;
+
+namespace Enum_Display;
+
+public static class Program
+{
+    public static void Main(String[] args)
+    {
+        Customers customers = new();
+        customers.DisplayName();
+    }
+}
